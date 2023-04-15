62394a5b42aebae25a1e2b07ede622a5eb676611
bartosz.sz <bartosz.szkiladz@gmail.com>
2023-04-15 21:24:05 +0200

    added pagination

--- src/index.js
+++ src/index.js
@@ -1,42 +1,83 @@
 import Notiflix from 'notiflix';
+import axios from 'axios';
 
-// const searchFormEl = document.getElementById('search-form');
 const searchInputEl = document.querySelector('#search-form input');
 const searchButtonEl = document.querySelector('#search-form button');
 const pictureEl = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
 
-const API_URL = 'https://pixabay.com/api/';
+loadMoreBtn.style.visibility = 'hidden';
+let page = 1;
+let pageShow = 40;
 
-const apiSearch = () => {
-  const searchValue = searchInputEl.value;
+const apiSearch = async () => {
+  const API_URL = 'https://pixabay.com/api/';
 
-  const searchParams = new URLSearchParams({
-    key: '35294695-6bfc4b24db5372eaae3354bab',
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-    q: searchValue,
+  const response = await axios.get(API_URL, {
+    params: {
+      key: '35294695-6bfc4b24db5372eaae3354bab',
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      q: searchInputEl.value,
+      per_page: pageShow,
+      page: page,
+    },
   });
 
-  fetch(`${API_URL}?${searchParams}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(response.status);
-      }
-      return response.json();
-    })
+  return response;
+};
+
+const fetchPhotos = () => {
+  apiSearch()
     .then(pictures => {
-      showPictures(pictures);
+      const totalHits = pictures.data.total;
+
+      if (pictures.data.hits.length === 0) throw new Error();
+
+      pictureEl.innerHTML = showPictures(pictures);
+
+      totalHits > 40
+        ? (loadMoreBtn.style.visibility = 'visible')
+        : (loadMoreBtn.style.visibility = 'hidden');
+
+      console.log(totalHits);
+
+      Notiflix.Notify.info(`Hooray! We found ${totalHits} images.`);
     })
     .catch(error => {
-      console.log(error);
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    });
+};
+
+const fetchNewPhotos = () => {
+  apiSearch()
+    .then(pictures => {
+      const totalHits = pictures.data.total;
+
+      pictureEl.insertAdjacentHTML('beforeend', showPictures(pictures));
+
+      totalHits / page > 40
+        ? (loadMoreBtn.style.visibility = 'visible')
+        : (loadMoreBtn.style.visibility = 'hidden');
+
+      console.log(totalHits / page);
+    })
+    .catch(error => {
+      Notiflix.Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
     });
 };
 
 const showPictures = pictures => {
-  const drawPicture = pictures.hits.map(picture => {
-    return `
-    <div class="photo-card border rounded shadow-lg mt-5">
+  return (drawPicture = pictures.data.hits
+    .map(
+      picture =>
+        `
+    <div class="photo-card border rounded shadow-lg">
       <img src="${picture.webformatURL}" alt="${picture.tags}" loading="lazy" class="w-80 h-52 object-cover" />
         <div class="info p-3 text-sm flex justify-between items-center">
           <p class="info-item flex flex-col text-center">
@@ -56,19 +97,22 @@ const showPictures = pictures => {
             ${picture.downloads}
           </p>
         </div>
-    </div>`;
-  });
-
-  if (drawPicture.length === 0) {
-    return Notiflix.Notify.failure(
-      'Sorry, there are no images matching your search query. Please try again.'
-    );
-  }
-
-  pictureEl.innerHTML = drawPicture.join('');
+    </div>`
+    )
+    .join(''));
 };
 
 searchButtonEl.addEventListener('click', e => {
+  page = 1;
   e.preventDefault();
-  apiSearch();
+  fetchPhotos();
+
+  console.log('first button', page);
+});
+
+loadMoreBtn.addEventListener('click', () => {
+  page++;
+  fetchNewPhotos();
+
+  console.log('second button', page);
 });
